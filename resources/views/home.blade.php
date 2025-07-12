3f0014fa74e10a43b0d2aaa4e08f4a3e39775ef4
Hamza Abu Hatab <habuhatab3@smail.ucas.edu.ps>
2025-07-12 15:08:01 +0300

    phase 3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,24 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <h1 class="mb-4">Home Page - Dashpoard</h1>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+    <div class="row">
+        <div class="col-md-6 mb-3">
+            <a href="{{ route('users.index') }}" class="btn btn-primary btn-block">
+                go to user managment
+            </a>
+        </div>
+        <div class="col-md-6 mb-3">
+            <a href="{{ route('requests.index') }}" class="btn btn-success btn-block">
+                go to requests managment
+            </a>
         </div>
     </div>
 </div>
